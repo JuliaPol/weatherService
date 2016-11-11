81e42080468fd53aef61ac6c2fa77f32efa30042
Julia Polushina <287jpi@gmail.com>
2016-11-11 23:46:08 +0300

    1/1

--- src/main/java/weather/Server.java
+++ src/main/java/weather/Server.java
@@ -1,19 +1,28 @@
 package weather;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.github.fedy2.weather.data.Condition;
+
+import javax.xml.bind.JAXBException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import static java.lang.Thread.NORM_PRIORITY;
 
 /**
  * Created by Julia on 20.10.2016.
  */
-public class Server extends Thread{
+public class Server extends Thread {
     private Socket socket;
     private int num;
 
@@ -22,7 +31,6 @@ public class Server extends Thread{
         try {
             int i = 0; // счётчик подключений
 
-            // привинтить сокет на локалхост, порт 3128
             ServerSocket ss = new ServerSocket(port); // создаем сокет сервера и привязываем его к вышеуказанному порту
             System.out.println("Ожидаем клиентов");
 
@@ -35,27 +43,27 @@ public class Server extends Thread{
                 i++;
             }
         } catch (Exception e) {
-            System.out.println("init error: " + e); // вывод исключений
+            System.out.println("Ошибка: " + e); // вывод исключений
             e.printStackTrace();
         }
     }
 
-    public Server(int num, Socket s)
-    {
+    public Server(int num, Socket s) {
         // копируем данные
         this.num = num;
         this.socket = s;
 
-        // и запускаем новый вычислительный поток (см. ф-ю run())
+        // и запускаем новый вычислительный поток
         setDaemon(true);
         setPriority(NORM_PRIORITY);
         start();
     }
 
-    public void run()
-    {
-        try
-        {
+    public void run() {
+        try {
+            String responseToClient = null;
+            String request = null;
+
             // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиенту.
             InputStream sin = socket.getInputStream();
             OutputStream sout = socket.getOutputStream();
@@ -64,16 +72,84 @@ public class Server extends Thread{
             DataInputStream in = new DataInputStream(sin);
             DataOutputStream out = new DataOutputStream(sout);
 
-            String line = null;
-            line = in.readUTF(); // ожидаем пока клиент пришлет строку текста.
-            System.out.println("Клиент запросил " + line);
-            String line1 = Weather.weather(line);
-            System.out.println("Отсылаем клиенту " + line1);
-            out.writeUTF(line1); // отсылаем клиенту обратно ту самую строку текста.
+            request = in.readUTF(); // ожидаем пока клиент пришлет строку текста.
+            System.out.println("Клиент запросил " + request);
+            // check if cache/line.txt exist
+
+            String filePathString = "src/main/cache/" + request + ".txt";
+
+            List<String> resIfE = null; // response if file exists
+
+
+            if (new File(filePathString).isFile() && new File(filePathString).exists() && !new File(filePathString).isDirectory()) {
+                System.out.println("\nFile exists! Loading data!\n");
+                try {
+                    resIfE = Files.readAllLines(Paths.get(filePathString), Charset.defaultCharset());
+
+                    // date
+                    String stringDate = resIfE.get(0);
+                    String stringDateFormat = "EEE MMM dd HH:mm:ss z yyyy";
+                    SimpleDateFormat format = new SimpleDateFormat(stringDateFormat, Locale.US);
+                    Date fileDate = format.parse(stringDate);
+
+
+                    DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.US);
+                    Calendar cal = Calendar.getInstance();
+                    cal.add(Calendar.HOUR, -1);
+
+                    boolean needToUpdate = cal.getTime().after(fileDate);
+
+                    if (needToUpdate) {
+
+                        System.out.println("\nRequest to Yahoo to update data!\n");
+                        responseToClient = logWeather(filePathString, request);
+                    } else {
+                        responseToClient = resIfE.get(1);
+                    }
+
+                    System.out.println(responseToClient + " градусов по Цельсию");
+                } catch (Exception ex) {
+                    responseToClient = "error";
+                    ex.printStackTrace();
+                }
+            } else {
+                System.out.println("\nRequest to Yahoo!\n");
+                try {
+                    responseToClient = logWeather(filePathString, request);
+                } catch (NullPointerException ex) {
+                    responseToClient = "error";
+                    ex.printStackTrace();
+                }
+            }
+
+            System.out.println("Отсылаем клиенту " + responseToClient + " градусов по Цельсию");
+            out.writeUTF(responseToClient); // отсылаем клиенту обратно ту самую строку текста.
             out.flush(); // заставляем поток закончить передачу данных.
         } catch (Exception x) {
             x.printStackTrace();
         }
     }
+
+
+    public String logWeather(String filePathString, String request) throws java.io.FileNotFoundException {
+        File f = new File(filePathString);
+        PrintWriter outWriter = new PrintWriter(f);
+
+        Weather newWeather = null; // get weather
+        try {
+            newWeather = new Weather(request);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+            System.out.println("JAXBException");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException");
+        }
+
+        outWriter.println(newWeather.showDate().toString());
+        outWriter.println(Integer.toString(newWeather.showTemp()));
+        outWriter.close();
+        return Integer.toString(newWeather.showTemp());
+    }
 }
-//"2123260"
--- src/main/java/weather/Weather.java
+++ src/main/java/weather/Weather.java
@@ -2,25 +2,41 @@ package weather;
 
 import com.github.fedy2.weather.YahooWeatherService;
 import com.github.fedy2.weather.data.Channel;
+import com.github.fedy2.weather.data.Condition;
 import com.github.fedy2.weather.data.unit.DegreeUnit;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Julia on 20.10.2016.
  */
 public class Weather {
-    public static String weather(String idCity) throws JAXBException, IOException {
+    private Date date;
+    private int temp;
+
+
+    public Date showDate() {
+        return date;
+    }
+
+    public int showTemp() {
+        return temp;
+    }
+
+    public Weather() {
+        this.date = null;
+        this.temp = 0;
+    }
+
+    public Weather(String titleCity) throws JAXBException, IOException {
         YahooWeatherService service = new YahooWeatherService();
-        Channel result = service.getForecast(idCity, DegreeUnit.CELSIUS);
-        System.out.println(result.getDescription());
-        System.out.println(result.getTitle());
-        System.out.println(result.getWind());
-        System.out.println(result.getAtmosphere());
-        System.out.println(result.getLink());
-        System.out.println(result.getItem().getForecasts());
-        System.out.println(result.getItem().getCondition());
-        return result.getItem().getCondition().toString();
+        List<Channel> channels = service.getForecastForLocation(titleCity, DegreeUnit.CELSIUS).first(1);
+        for (Channel channel : channels) {
+            this.date = channel.getItem().getCondition().getDate();
+            this.temp = channel.getItem().getCondition().getTemp();
+        }
     }
 }
